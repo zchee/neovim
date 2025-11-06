9f797b410a6ed8310664255c7408041a43130d8b
Koichi Shiraishi <zchee.io@gmail.com>
2025-11-06 23:48:14 +0900

    test(tui): add flush diagnostic hooks

--- src/nvim/tui/tui.h
+++ src/nvim/tui/tui.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifdef UNIT_TESTING
+# include <uv.h>
+#endif
+
 #include "nvim/highlight_defs.h"  // IWYU pragma: keep
 #include "nvim/tui/terminfo_defs.h"
 #include "nvim/tui/tui_defs.h"  // IWYU pragma: keep
@@ -9,6 +13,20 @@
 size_t nvim_test_terminfo_copy_cached(TerminfoDef def, const char *str, bool parametric,
                                       size_t cached_len_in, size_t *cached_len_out,
                                       char *dst, size_t len);
+
+typedef struct {
+  size_t try_call_count;
+  size_t write_call_count;
+  size_t try_total;
+  size_t try_buf_lens[3];
+  size_t write_total;
+  size_t write_buf_lens[3];
+  bool final_invisible;
+} NvimTuiFlushDiag;
+
+NvimTuiFlushDiag nvim_test_tui_flush_diag(size_t payload_len, ssize_t try_result,
+                                          int write_ret, bool start_invisible,
+                                          bool want_invisible);
 #endif
 
 #include "tui/tui.h.generated.h"
