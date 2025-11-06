27c20c8f5f277cf2b4afcb9c1643c28c34a7eea3
Koichi Shiraishi <zchee.io@gmail.com>
2025-11-06 10:58:58 +0900

    test(terminfo): cover cached cursor copy helper

--- test/unit/terminfo_spec.lua
+++ test/unit/terminfo_spec.lua
@@ -2,6 +2,8 @@ local t = require('test.unit.testutil')
 local itp = t.gen_itp(it)
 
 local terminfo = t.cimport('./src/nvim/tui/terminfo.h')
+local terminfo_defs = t.cimport('./src/nvim/tui/terminfo_defs.h')
+local tui = t.cimport('./src/nvim/tui/tui.h')
 
 describe('terminfo_is_parametric', function()
   itp('detects simple constant sequences', function()
@@ -21,3 +23,46 @@ describe('terminfo_is_parametric', function()
   end)
 end)
 
+
+describe('terminfo cached copy helper', function()
+  local ffi = require('ffi')
+
+  itp('copies non-parametric capability from cache when it fits', function()
+    local buf = ffi.new('char[?]', 32)
+    local cached_len = ffi.new('size_t[1]', 0)
+
+    local seq = '\027[?25l'
+    local written = tui.nvim_test_terminfo_copy_cached(
+      terminfo_defs.kTerm_cursor_invisible,
+      seq,
+      false,
+      0,
+      cached_len,
+      buf,
+      32
+    )
+
+    t.eq(#seq, written)
+    t.eq(seq, ffi.string(buf, written))
+    t.eq(#seq, cached_len[0])
+  end)
+
+  itp('falls back when buffer is too small', function()
+    local buf = ffi.new('char[?]', 4)
+    local cached_len = ffi.new('size_t[1]', 0)
+
+    local seq = '\027[?25l'
+    local written = tui.nvim_test_terminfo_copy_cached(
+      terminfo_defs.kTerm_cursor_invisible,
+      seq,
+      false,
+      0,
+      cached_len,
+      buf,
+      4
+    )
+
+    t.eq(0, written)
+    t.eq(#seq, cached_len[0])
+  end)
+end)
