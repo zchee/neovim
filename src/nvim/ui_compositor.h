b93d4549b1c5fb35d57f2e5572b577c0d49a111e
Koichi Shiraishi <zchee.io@gmail.com>
2025-11-12 09:01:41 +0900

    ui: avoid compositor fallback for hidden popup grid

--- src/nvim/ui_compositor.h
+++ src/nvim/ui_compositor.h
@@ -1,8 +1,32 @@
 #pragma once
 
+#include <stdint.h>
+
 #include "nvim/api/private/defs.h"  // IWYU pragma: keep
 #include "nvim/grid_defs.h"  // IWYU pragma: keep
 #include "nvim/types_defs.h"  // IWYU pragma: keep
 #include "nvim/ui_defs.h"  // IWYU pragma: keep
+
+typedef struct {
+  uint64_t fallback_calls;
+  uint64_t fallback_span_total;
+  uint64_t fallback_recomposed_total;
+  uint64_t fallback_width_total;
+  uint64_t fallback_blending_calls;
+  uint64_t fallback_covered_calls;
+  uint64_t fallback_above_msg_calls;
+  uint64_t fallback_skipped_lines_total;
+  uint64_t fallback_cover_handle_last;
+  int64_t fallback_last_row_checked;
+  int64_t fallback_msg_row;
+  int64_t fallback_rows_argument;
+  int64_t fallback_cover_zindex_last;
+  uint64_t fallback_popup_calls;
+  uint64_t fallback_popup_ignored;
+  uint64_t fallback_external_calls;
+} UICompMetrics;
+
+void ui_comp_metrics_snapshot(UICompMetrics *out);
+void ui_comp_metrics_reset(void);
 
 #include "ui_compositor.h.generated.h"
